84a69d02412c9793156b4e2048ca52e9e3c7b52e
王海涛 <15124505701@163.com>
2017-09-19 09:29:10 +0800

    切换登录和注册

---
+++ my-app/src/leanCloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage'
+  var APP_ID = "AAyPdEhvRwHSGDCIf8oYydt7-gzGzoHsz"
+  var APP_KEY = "GT54wCheXhkqB0HrWozHmepJ"
+  AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+ })
+ export default AV
